42ec934ac937d91a22358f0687f5e83b26a2d116
Ryan Nowak <nowakra@gmail.com>
2015-08-28 09:05:52 -0700

    Make ModelBindingResult a struct.

--- src/Microsoft.AspNet.Mvc.Abstractions/ModelBinding/ModelBindingResult.cs
+++ src/Microsoft.AspNet.Mvc.Abstractions/ModelBinding/ModelBindingResult.cs
@@ -1,47 +1,35 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
 namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     /// <summary>
     /// Contains the result of model binding.
     /// </summary>
-    public class ModelBindingResult
+    public struct ModelBindingResult : IEquatable<ModelBindingResult>
     {
-        /// <summary>
-        /// Creates a new <see cref="ModelBindingResult"/> indicating a fatal error.
-        /// </summary>
-        /// <param name="key">The key using which was used to attempt binding the model.</param>
-        public ModelBindingResult(string key)
+        public static readonly ModelBindingResult NoResult = new ModelBindingResult();
+
+        public static readonly Task<ModelBindingResult> NoResultTask = Task.FromResult(NoResult);
+
+        public static ModelBindingResult Failed(string key)
+        {
+            return new ModelBindingResult(key, model: null, isModelSet: false, validationNode: null);
+        }
+
+        public static ModelBindingResult Success(string key, object model, ModelValidationNode validationNode)
+        {
+            return new ModelBindingResult(key, model, isModelSet: true, validationNode: validationNode);
+        }
+
+        private ModelBindingResult(string key, object model, bool isModelSet, ModelValidationNode validationNode)
         {
             Key = key;
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="ModelBindingResult"/>.
-        /// </summary>
-        /// <param name="model">The model which was created by the <see cref="IModelBinder"/>.</param>
-        /// <param name="key">The key using which was used to attempt binding the model.</param>
-        /// <param name="isModelSet">A value that represents if the model has been set by the
-        /// <see cref="IModelBinder"/>.</param>
-        public ModelBindingResult(object model, string key, bool isModelSet)
-            : this(model, key, isModelSet, validationNode: null)
-        {
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="ModelBindingResult"/>.
-        /// </summary>
-        /// <param name="model">The model which was created by the <see cref="IModelBinder"/>.</param>
-        /// <param name="key">The key using which was used to attempt binding the model.</param>
-        /// <param name="isModelSet">A value that represents if the model has been set by the
-        /// <see cref="IModelBinder"/>.</param>
-        /// <param name="validationNode">A <see cref="ModelValidationNode"/> which captures the validation information.
-        /// </param>
-        public ModelBindingResult(object model, string key, bool isModelSet, ModelValidationNode validationNode)
-        {
             Model = model;
-            Key = key;
             IsModelSet = isModelSet;
             ValidationNode = validationNode;
         }
@@ -76,5 +64,54 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
         /// A <see cref="ModelValidationNode"/> associated with the current <see cref="ModelBindingResult"/>.
         /// </summary>
         public ModelValidationNode ValidationNode { get; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as ModelBindingResult?;
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Equals(other.Value);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            if (Key == null)
+            {
+                return 0;
+            }
+            else if (IsModelSet)
+            {
+                Debug.Assert(Key != null);
+                return Key.GetHashCode() * Model?.GetHashCode() ?? 17;
+            }
+            else
+            {
+                Debug.Assert(Key != null);
+                return Key.GetHashCode();
+            }
+        }
+
+        public bool Equals(ModelBindingResult other)
+        {
+            return
+                string.Equals(Key, other.Key, StringComparison.Ordinal) &&
+                IsModelSet == other.IsModelSet &&
+                object.Equals(Model, other.Model);
+        }
+
+        public static bool operator ==(ModelBindingResult x, ModelBindingResult y)
+        {
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(ModelBindingResult x, ModelBindingResult y)
+        {
+            return !x.Equals(y);
+        }
     }
 }
--- src/Microsoft.AspNet.Mvc.Core/ModelBinding/ArrayModelBinder.cs
+++ src/Microsoft.AspNet.Mvc.Core/ModelBinding/ArrayModelBinder.cs
@@ -21,7 +21,7 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
         {
             if (bindingContext.ModelMetadata.IsReadOnly)
             {
-                return Task.FromResult<ModelBindingResult>(null);
+                return ModelBindingResult.NoResultTask;
             }
 
             return base.BindModelAsync(bindingContext);
